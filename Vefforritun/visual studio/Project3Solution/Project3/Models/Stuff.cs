bd989121c8e81265408b0acbb7eaea098159b607
Victor Gressier <victor15@ru.is>
2016-02-28 15:29:32 +0000

    eitthvad

--- Vefforritun/visual studio/Project3Solution/Project3/Models/Stuff.cs
+++ Vefforritun/visual studio/Project3Solution/Project3/Models/Stuff.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,16 @@ namespace Project3.Models
 	public class Stuff
 	{
 		public int ID { get; set; }
+
 		public string Name { get; set; }
+		[Required(ErrorMessage = "Name is required!")]
+
 		public string Description { get; set; }
+
 		public int Available { get; set; }
+
 		public float Price { get; set; }
+
 		public int Category { get; set; }
 
 		internal static void ForEach(Func<object, Stuff> p)
